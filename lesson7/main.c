f78a56e6470da901ab969425518b2e2384ae09ae
wisang <wseom7@gmail.com>
2023-02-05 20:07:55 +0900

    lesson 7 complete

--- lesson7/main.c
+++ lesson7/main.c
@@ -6,22 +6,20 @@
 
 int main() {
     SYSCTL_RCGCGPIO_R |= (1U << 5);  /* enable clock for GPIOF */
-    SYSCTL_GPIOHBCTL_R |= (1u << 5);
-    GPIO_PORTF_AHB_DIR_R |= (LED_BLUE | LED_GREEN | LED_RED); // set pins 1,2, and 3 as outputs
-    GPIO_PORTF_AHB_DEN_R |= (LED_BLUE | LED_GREEN | LED_RED);
+    GPIO_PORTF_DIR_R |= (LED_BLUE | LED_GREEN | LED_RED); // set pins 1,2, and 3 as outputs
+    GPIO_PORTF_DEN_R |= (LED_BLUE | LED_GREEN | LED_RED);
     
-    GPIO_PORTF_AHB_DATA_BITS_R[LED_BLUE] = LED_BLUE;
+    GPIO_PORTF_DATA_R |= LED_BLUE;
 
     while (1) {
-        GPIO_PORTF_AHB_DATA_BITS_R[LED_RED] = LED_RED;
+        GPIO_PORTF_DATA_R |= LED_RED;
 
         int volatile counter = 0;
         while (counter < 1000000) {  // delay loop
             ++counter;
         }
 
-        //GPIO_PORTF_DATA_R &= ~LED_RED;
-        GPIO_PORTF_AHB_DATA_BITS_R[LED_RED] = 0;
+        GPIO_PORTF_DATA_R &= ~LED_RED;
         counter = 0;
         while (counter < 1000000) {  // delay loop
             ++counter;
