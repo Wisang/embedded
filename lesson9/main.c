cd102e45fdf8fc03307adcc3d582c3f96f593182
wisang <wseom7@gmail.com>
2023-02-12 22:57:33 +0900

    lesson 9 complete

---
+++ lesson9/main.c
@@ -0,0 +1,28 @@
+#include "lm4f120h5qr.h"
+#include "delay.h"
+
+#define LED_RED (1U << 1)
+#define LED_GREEN (1U << 2)
+#define LED_BLUE (1U << 3)
+
+int main() {
+    SYSCTL_RCGCGPIO_R |= (1U << 5);  /* enable clock for GPIOF */
+    SYSCTL_GPIOHBCTL_R |= (1u << 5);
+    GPIO_PORTF_AHB_DIR_R |= (LED_BLUE | LED_GREEN | LED_RED); // set pins 1,2, and 3 as outputs
+    GPIO_PORTF_AHB_DEN_R |= (LED_BLUE | LED_GREEN | LED_RED);
+    
+    GPIO_PORTF_AHB_DATA_BITS_R[LED_BLUE] = LED_BLUE;
+
+    while (1) {
+        GPIO_PORTF_AHB_DATA_BITS_R[LED_RED] = LED_RED;
+        
+        delay(1000000);
+
+        //GPIO_PORTF_DATA_R &= ~LED_RED;
+        GPIO_PORTF_AHB_DATA_BITS_R[LED_RED] = 0;
+        
+        delay(500000);
+
+    }
+    return 0;
+}
